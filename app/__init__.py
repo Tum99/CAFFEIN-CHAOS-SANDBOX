c30724ad092aa39771f5ddfa74211afe49682fa1
Tum99 <faithtum92@gmail.com>
2026-07-03 13:06:35 +0300

    Imports time

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager, current_user
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
+import time
 
 import os
 
